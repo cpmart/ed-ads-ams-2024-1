ffa070d82d02aab6ea41153ab5d26fcc8d592a01
cpmart <cpmart@gmail.com>
2024-02-26 15:49:55 -0300

    Tipos de Dados

---
+++ ex01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    char caractere = 'X';
+    printf("A letra é %c\n",caractere);
+
+    short numeroShort = 33000;
+    printf("O número inteiro (short) é: %hd\n",numeroShort);
+    int numeroInt = 4000000000;
+    printf("O número inteiro  (int) é: %d\n",numeroInt);
+    long numeroLong = 4000000000;
+    printf("O número inteiro (long) é: %ld\n",numeroLong);
+    
+}
