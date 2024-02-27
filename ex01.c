71bfca9924972396e21b32a2c3422bce9761b4aa
cpmart <cpmart@gmail.com>
2024-02-27 14:33:16 -0300

    Constante máximo e mínimo

--- ex01.c
+++ ex01.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
 
 int main(){
-    char caractere = 'X';
-    printf("A letra é %c\n",caractere);
+    //Constantes que apresentam os mínimos e máximos em cada tipo inteiro.
 
-    short numeroShort = 33000;
-    printf("O número inteiro (short) é: %hd\n",numeroShort);
-    int numeroInt = 4000000000;
-    printf("O número inteiro  (int) é: %d\n",numeroInt);
-    long numeroLong = 4000000000;
-    printf("O número inteiro (long) é: %ld\n",numeroLong);
+    system("clear");    //Limpar tela, utilizando a biblioteca stdlib.h
     
+    //Caractere         //implementado na biblioteca limits.h
+    printf("Valor mínimo para char: %d\n", CHAR_MIN);
+    printf("Valor máximo para char: %d\n\n", CHAR_MAX);
+    
+    //INTEIRO           //implementado na biblioteca limits.h
+    printf("Valor máximo para short: %hd\n", SHRT_MAX);
+    printf("Valor mínimo para short: %hd\n\n", SHRT_MIN);
+    
+    printf("Valor máximo para int: %d\n", INT_MAX);
+    printf("Valor mínimo para int: %d\n\n", INT_MIN);
+    
+    printf("Valor máximo para long: %ld\n", LONG_MAX);
+    printf("Valor mínimo para long: %ld\n\n", LONG_MIN);
+    
+    //REAL              //implementado na biblioteca float.h
+    printf("Valor máximo para float: %e\n", FLT_MAX);
+    printf("Valor mínimo para float: %e\n\n", FLT_MIN);
+    printf("Valor máximo para double: %e\n", DBL_MAX);
+    printf("Valor mínimo para double: %e\n", DBL_MIN);
 }
