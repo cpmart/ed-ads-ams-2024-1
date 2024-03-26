57c30b568a5d6b1c602c906abf62144e5193d374
cpmart <cpmart@gmail.com>
2024-03-26 15:38:11 -0300

    ponteiro2

--- ex08-piramide.c
+++ ex08-piramide.c
@@ -3,15 +3,15 @@
 
 int main(){
     int n;
-    printf("Digite a altura da pirâmide entre 1 e 10: ");
+    printf("Digite a altura da pirâmide entre 1 e 20: ");
     scanf("%d",&n);
-    for(int i=1; i<n; i++){
-        for(int j=i; j<=n; j++){
+    for(int i=1; i<=n; i++){
+        for(int j=i-1; j<=n; j++){
             printf(" ");
         }
         for(int j=1; j<=i; j++){
             printf("%d ",i);
-        }
+        } 
         printf("\n");
     }
 }
